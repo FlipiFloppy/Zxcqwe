b4a9983ee7ba3520bb40fe348bca03a79298888c
unknown <iluxatop228@gmail.com>
2024-03-15 15:55:38 +0300

    adsd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Максимов Илья</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/projects" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            {#each nav as link}
+            {#if link.isShow}
+                <a href={link.href} class="link">{link.title}</a>
+                <span>&nbsp;&nbsp;&nbsp;</span>
+            {/if}
+        {/each}
         </div>
     </div>
 </nav>
@@ -15,6 +17,35 @@
     <slot></slot>
 </div>
 
+<script>
+    const nav = [
+        {
+         title: "Обо мне",
+         href: "/about",
+         isShow: true,
+        },
+        {
+         title: "Блог",
+         href: "/blog",
+         isShow: true,
+        },
+        {
+         title: "Проект",
+         href: "/project",
+         isShow: true,
+        },
+        {
+         title: "Контакты",
+         href: "/contacts",
+         isShow: true,
+        },
+        {
+         title: "temp",
+         href: "/temp",
+         isShow: false,
+        },
+    ];
+</script>
 <style>
     .container {
     max-width: 1400px;
